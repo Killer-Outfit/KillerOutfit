b4612a10ef2890bd4f94c33a67665c0b51e13eb5
SpessMareen <eramitch@ucsc.edu>
2019-04-21 23:00:29 -0700

    Added attack hit sounds (player->enemy) for each clothing part, and added a prefab that plays randomly selected spatial ambient sounds

---
+++ Assets/Scripts/Ambient Sounds/ambientDelay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ambientDelay : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] sounds;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            AudioClip clip = GetRandomClip();
+            audioSource.clip = clip;
+            audioSource.PlayDelayed(Random.Range(5, 10));
+        }
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return sounds[UnityEngine.Random.Range(0, sounds.Length)];
+    }
+}
