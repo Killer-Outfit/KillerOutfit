9bbc3ba1209bceda82baa77e5243dc065364a296
SpessMareen <eramitch@ucsc.edu>
2019-04-28 19:32:37 -0700

    Created basic shop menu UI and started a script that controls the shop

---
+++ Assets/Scripts/UI/ShopScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShopScript : MonoBehaviour, IPointerEnterHandler, ISelectHandler
+{
+
+    GameObject selectedItem;
+    public Text itemName;
+    public Text itemDesc;
+    public Image itemVid;
+    int[,] itemArr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemArr = new int[3,3];
+        itemName = GetComponent<Text>();
+        itemDesc = GetComponent<Text>();
+        itemVid = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+}
