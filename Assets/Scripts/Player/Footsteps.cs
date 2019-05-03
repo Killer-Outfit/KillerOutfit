861ea0de94d2b82d02440c69bf7aa082965ce97f
SpessMareen <eramitch@ucsc.edu>
2019-05-02 17:34:39 -0700

    re-added footstep sounds back into player movement

--- Assets/Scripts/Player/Footsteps.cs
+++ Assets/Scripts/Player/Footsteps.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class footsteps : MonoBehaviour
+public class Footsteps : MonoBehaviour
 {
     [SerializeField]
     private AudioClip[] footstepSounds;
