301f6e0b12978552df089fe17494dddcb4076e20
SpessMareen <eramitch@ucsc.edu>
2019-05-03 22:06:56 -0700

    Edited the music to be softer, added colors for enemy 1 and 2, and added enemy 1 to the scene for the gameplay demo

--- Assets/Scripts/Enemies/HandProjMove.cs
+++ Assets/Scripts/Enemies/HandProjMove.cs
@@ -37,7 +37,7 @@ public class HandProjMove : MonoBehaviour
             hit = true;
             c.gameObject.GetComponent<playerNew>().decreaseHealth(damage);
             Debug.Log("Hit player");
-            c.gameObject.GetComponent<playerNew>().decreaseHealth(50f);
+            c.gameObject.GetComponent<playerNew>().decreaseHealth(5f);
         }
     }
 }
