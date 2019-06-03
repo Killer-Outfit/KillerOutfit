1028d088b886e905ef7d84ff7b3b689b07e84459
SpessMareen <eramitch@ucsc.edu>
2019-06-02 18:46:14 -0700

    Remade cutscenes and added text for the scenes

--- Assets/Scripts/Systems/StartGame.cs
+++ Assets/Scripts/Systems/StartGame.cs
@@ -9,6 +9,21 @@ public class StartGame : MonoBehaviour
     public AudioClip clip;
     public AudioClip clip2;
 
+    [SerializeField]
+    private GameObject[] cutscenes;
+
+    [System.Serializable]
+    public class cutsceneSub
+    {
+        public int parentScene;
+        public GameObject[] subScenes;
+    }
+
+    public cutsceneSub sub1;
+    public cutsceneSub sub2;
+    public string[] cutsceneText;
+    public GameObject textBox;
+
     FMOD.Studio.EventInstance music;
 
     CanvasGroup canvas;
@@ -21,30 +36,34 @@ public class StartGame : MonoBehaviour
     bool nextScene;
     bool cutsceneDone;
     AudioSource source;
-    GameObject[] cutscenes = new GameObject[6];
     SpriteRenderer ler_p;
+    SpriteRenderer subLerp;
+    SpriteRenderer[] lerps;
     float tim;
+
+    private Text activeMessage;
 
     void Awake()
     {
         music = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Main Menu");
         music.start();
         Time.timeScale = 1.0f;
+        activeMessage = GameObject.Find("sceneText").GetComponent<Text>();
+        activeMessage.enabled = false;
+        textBox.SetActive(false);
         canvas = GameObject.Find("MainMenuCanvas").GetComponent<CanvasGroup>();
         startButton = GameObject.Find("Start Game (Button)").GetComponent<Button>();
         start = GameObject.Find("Start Game (Button)");
         quitButton = GameObject.Find("Exit (Button)").GetComponent<Button>();
         quit = GameObject.Find("Exit (Button)");
         title = GameObject.Find("Title image - Orange_green");
-        cutscenes[0] = GameObject.Find("sketch1");
-        cutscenes[1] = GameObject.Find("sketch2");
-        cutscenes[2] = GameObject.Find("sketch3");
-        cutscenes[3] = GameObject.Find("sketch4");
-        cutscenes[4] = GameObject.Find("sketch5");
-        cutscenes[5] = GameObject.Find("sketch6");
         source = GameObject.Find("MainMenuCanvas").GetComponent<AudioSource>();
         ler_p = GameObject.Find(cutscenes[1].name).GetComponent<SpriteRenderer>();
         foreach (GameObject i in cutscenes)
+            i.SetActive(false);
+        foreach (GameObject i in sub1.subScenes)
+            i.SetActive(false);
+        foreach (GameObject i in sub2.subScenes)
             i.SetActive(false);
         fadeDone = false;
         nextScene = false;
@@ -78,13 +97,34 @@ public class StartGame : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
-    IEnumerator FadeOut(GameObject scene)
+    IEnumerator FadeOut(GameObject scene, int mainScene)
     {
         //startButton.interactable = false;
         //quitButton.interactable = false;
         tim = 0f;
         while (ler_p.color != Color.black)
         {
+            if (mainScene == sub1.parentScene - 1)
+            {
+                for (int i = 0; i < sub1.subScenes.Length; i++)
+                {
+                    //Debug.Log(i);
+                    lerps = new SpriteRenderer[sub1.subScenes.Length];
+                    lerps[i] = sub1.subScenes[i].GetComponent<SpriteRenderer>();
+                    StartCoroutine(FadeSub(lerps[i], sub1.subScenes[i]));
+                    //sub1.subScenes[i].SetActive(false);
+                }
+            }
+            if (mainScene == sub2.parentScene - 1)
+            {
+                for (int i = 0; i < sub2.subScenes.Length; i++)
+                {
+                    lerps = new SpriteRenderer[sub2.subScenes.Length];
+                    lerps[i] = sub2.subScenes[i].GetComponent<SpriteRenderer>();
+                    StartCoroutine(FadeSub(lerps[i], sub2.subScenes[i]));
+                    //sub2.subScenes[i].SetActive(false);
+                }
+            }
             //Debug.Log("alpha is " + canvas.alpha);
             ler_p.color = Color.Lerp(Color.white, Color.black, tim);
             tim += Time.deltaTime;
@@ -103,24 +143,46 @@ public class StartGame : MonoBehaviour
         yield return null;
     }
 
+    private IEnumerator FadeSub(SpriteRenderer piece, GameObject piecePart)
+    {
+        while (piece.color != Color.black)
+        {
+            //Debug.Log("alpha is " + canvas.alpha);
+            piece.color = Color.Lerp(Color.white, Color.black, tim);
+            //Debug.Log("time is " + tim);
+            //Debug.Log("1/2 delta time is " + Time.deltaTime / 2);
+            if (piece.color == Color.black)
+            {
+                piecePart.SetActive(false);
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
+    }
+
     private IEnumerator PlayCutscene()
     {
-        Debug.Log("cutscene started");
+        activeMessage.enabled = true;
+        textBox.SetActive(true);
+        //Debug.Log("cutscene started");
         for (int i = 0; i < cutscenes.Length; i++)
         {
-            Debug.Log("cutscene " + i + " active");
+            //Debug.Log("cutscene " + i + " active");
             cutscenes[i].SetActive(true);
             //Debug.Log(ler_p);
+            activeMessage.text = cutsceneText[i];
             ler_p = GameObject.Find(cutscenes[i].name).GetComponent<SpriteRenderer>();
-            Debug.Log(cutscenes[i].name);
+            //Debug.Log(cutscenes[i].name);
             ler_p.color = Color.black;
             nextScene = false;
-            yield return StartCoroutine(NextCutscene());
+            yield return StartCoroutine(NextCutscene(i));
             if (i == cutscenes.Length-1)
             {
+                activeMessage.enabled = false;
+                textBox.SetActive(false);
                 music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
                 source.PlayOneShot(clip2);
-                Debug.Log("reached end of cutscenes");
+                //Debug.Log("reached end of cutscenes");
                 cutsceneDone = true;
                 //StartCoroutine(LoadAsync());
             }
@@ -128,11 +190,11 @@ public class StartGame : MonoBehaviour
             {
                 source.PlayOneShot(clip);
             }
-            yield return StartCoroutine(FadeOut(cutscenes[i]));
+            yield return StartCoroutine(FadeOut(cutscenes[i], i));
         }
     }
 
-    private IEnumerator NextCutscene()
+    private IEnumerator NextCutscene(int mainScene)
     {
         tim = 0f;
         while (ler_p.color != Color.white)
@@ -147,6 +209,15 @@ public class StartGame : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
 
+        if(mainScene == sub1.parentScene - 1)
+        {
+            yield return StartCoroutine(NextSubScene(sub1));
+        }
+        if(mainScene == sub2.parentScene - 1)
+        {
+            yield return StartCoroutine(NextSubScene(sub2));
+        }
+
         bool done = false;
         while (!done) // essentially a "while true", but with a bool to break out naturally
         {
@@ -159,6 +230,29 @@ public class StartGame : MonoBehaviour
             }
             yield return null;
         }
+    }
+
+    private IEnumerator NextSubScene(cutsceneSub sub)
+    {
+        for(int i = 0; i < sub.subScenes.Length; i++)
+        {
+            sub.subScenes[i].SetActive(true);
+            subLerp = sub.subScenes[i].GetComponent<SpriteRenderer>();
+            subLerp.color = Color.black;
+            tim = 0f;
+            while (subLerp.color != Color.white)
+            {
+                //Debug.Log("alpha is " + canvas.alpha);
+                subLerp.color = Color.Lerp(Color.black, Color.white, tim);
+                tim += Time.deltaTime;
+                //Debug.Log("time is " + tim);
+                if (tim > 1)
+                    tim = 1;
+                //Debug.Log("1/2 delta time is " + Time.deltaTime / 2);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        yield return null;
     }
 
     private IEnumerator LoadAsync()
