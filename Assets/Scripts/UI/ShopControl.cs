5bc47a9196659f3ced8dedd02da78335e7c96ea8
Evan Blank <eblank@ucsc.edu>
2019-06-03 00:58:21 -0700

    added shop menu light flashes, model rotation, and background

--- Assets/Scripts/UI/ShopControl.cs
+++ Assets/Scripts/UI/ShopControl.cs
@@ -13,10 +13,19 @@ public class ShopControl : MonoBehaviour
     private GameObject resume;
     private GameObject player;
     public bool openable;
-
+    private GameObject[] papCams;
+    private int curFrame;
+    private int nextFrameEvent;
+    private int amountOfFlashes;
+    private List<Color> flashColors;
+    //private Canvas shopCanvas;
     // Start is called before the first frame update
     void Awake()
     {
+        curFrame = 0;
+        nextFrameEvent = 0;
+        amountOfFlashes = 0;
+        papCams = GameObject.FindGameObjectsWithTag("flash");
         player = GameObject.Find("PlayerBody");
         openable = false;
         can = GameObject.Find("Main Canvas").GetComponent<Canvas>();
@@ -25,13 +34,36 @@ public class ShopControl : MonoBehaviour
         shopResume = GameObject.Find("ResumeGame_ShopMenu (Button)");
         resume = GameObject.Find("ResumeGame (Button)");
         shopResume.SetActive(false);
-        shopOpen = true;
+        shopOpen = false;
+        flashColors = new List<Color>();
+        flashColors.Add(new Color(124f, 255f, 205f, 255f));
+        flashColors.Add(new Color(214f, 124f, 205f, 255f));
+        flashColors.Add(new Color(255f, 124f, 236f, 255f));
+        flashColors.Add(new Color(255f, 142f, 124f, 255f));
+        flashColors.Add(new Color(255f, 197f, 124f, 255f));
+        flashColors.Add(new Color(236f, 255f, 124f, 255f));
+        //shopCanvas = GameObject.Find("ShopCanvas").GetComponent<Canvas>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
+       
+        Debug.Log(curFrame.ToString() + " " + nextFrameEvent.ToString());
+        if (shopOpen && curFrame == nextFrameEvent)
+        {
+            Debug.Log("frames match");
+            curFrame = 0;
+            nextFrameEvent = Random.Range(15, 181);
+            amountOfFlashes = Random.Range(1, 4);
+            StartCoroutine("flashCameras");
+
+        }
+        else if (shopOpen)
+        {
+            curFrame += 1;
+        }
         if (Input.GetButtonDown("AButton") && openable)
 		{
 			maincam.enabled = !maincam.enabled;
@@ -40,6 +72,7 @@ public class ShopControl : MonoBehaviour
             shopOpen = !shopOpen;
             player.GetComponent<playerMove>().active = !player.GetComponent<playerMove>().active;
             player.GetComponent<playerNew>().active = !player.GetComponent<playerNew>().active;
+            //shopCanvas.enabled = !shopCanvas.enabled;
         }
 
         if (Input.GetKeyDown(KeyCode.Escape) && shopOpen == true)
@@ -63,6 +96,20 @@ public class ShopControl : MonoBehaviour
                 shopcam.enabled = true;
                 can.enabled = false;
             }
+        }
+    }
+
+    IEnumerator flashCameras()
+    {
+        for(int i = 0; i < amountOfFlashes; i++)
+        {
+            int flashIndex = Random.Range(0, papCams.Length);
+            float snapAngle = Random.Range(87f, 121f);
+            int colorIndex = Random.Range(0, flashColors.Count);
+            float waitTime = Random.Range(.05f, .3f);
+            Debug.Log("snapCam");
+            papCams[flashIndex].GetComponent<paparaziCamera>().snap(flashColors[colorIndex], snapAngle);
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
---
+++ Assets/paparaziCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paparaziCamera : MonoBehaviour
+{
+    public void snap(Color snapColor, float spotAngle)
+    {
+        GetComponent<Light>().color = snapColor;
+        GetComponent<Light>().spotAngle = spotAngle;
+        StartCoroutine("flash");
+    }
+    private IEnumerator flash()
+    {
+        Debug.Log(GetComponent<Light>().enabled);
+        GetComponent<Light>().enabled = true;
+        Debug.Log(GetComponent<Light>().enabled);
+        yield return new WaitForSeconds(.2f);
+        GetComponent<Light>().enabled = false;
+        Debug.Log(GetComponent<Light>().enabled);
+    }
+}
