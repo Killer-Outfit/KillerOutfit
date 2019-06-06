946f82c26f6919fe5f1c757e6ca0c28105ca7000
Eric Mitchell <eramitch@ucsc.edu>
2019-06-05 22:04:25 -0700

    Fixed issue with level 1 music playing when returning to main menu

--- Assets/Scripts/Systems/ReturnToMain.cs
+++ Assets/Scripts/Systems/ReturnToMain.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class ReturnToMain : MonoBehaviour
 {
+    FMOD.Studio.Bus masterBus;
+
+    void Start()
+    {
+        masterBus = FMODUnity.RuntimeManager.GetBus("Bus:/");
+    }
+
     public void ReturntoMain()
     {
+        masterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         SceneManager.LoadScene("MainMenu");
     }
 }
